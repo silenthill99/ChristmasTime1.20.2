52442e6a4d0ff0b75221ceaf866ce9bb519ff37e
BuildTools <unconfigured@null.spigotmc.org>
2024-05-25 11:13:14 +0200

    Mise à jour du projet

--- src/main/java/fr/silenthill99/christmas_time/data/models_and_blockstates/BlockStateGenerator.java
+++ src/main/java/fr/silenthill99/christmas_time/data/models_and_blockstates/BlockStateGenerator.java
@@ -3,11 +3,15 @@ package fr.silenthill99.christmas_time.data.models_and_blockstates;
 import fr.silenthill99.christmas_time.Main;
 import fr.silenthill99.christmas_time.init.ModBlocks;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockStateGenerator extends BlockStateProvider {
+
+    private ResourceLocation path;
+
     public BlockStateGenerator(PackOutput output, ExistingFileHelper exFileHelper) {
         super(output, Main.MODID, exFileHelper);
     }
